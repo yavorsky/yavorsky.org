5bf4d5f162279937dac7c0fbf480906e20a5e359
Artem Yavorskyi <aqson@me.com>
2025-02-18 16:06:26 +0000

    feat: fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,8 @@ import { getBlogPosts } from '@/app/blog/utils';
 export const baseUrl = 'https://portfolio-blog-starter.vercel.app';
 
 export default async function sitemap() {
-  const blogs = getBlogPosts().map((post) => ({
+  const posts = await getBlogPosts();
+  const blogs = posts.map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }));
