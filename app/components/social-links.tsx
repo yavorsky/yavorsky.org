12099756608bff4584c525692e5f66b161a7c158
Artem Yavorskyi <aqson@me.com>
2025-04-05 10:08:19 +0000

    fix: add bluesky logo

--- app/components/social-links.tsx
+++ app/components/social-links.tsx
@@ -5,10 +5,11 @@ import {
   MailIcon,
 } from 'lucide-react';
 import XIcon from './icons/x';
+import BlueSkyIcon from './icons/bluesky';
 
 export const SocialLinks = () => {
   return (
-    <div className="font-sm mt-4 flex  text-neutral-600 flex-row space-x-4 space-y-0 dark:text-neutral-300">
+    <div className="font-sm mt-4 flex items-center  text-neutral-600 flex-row space-x-4 space-y-0 dark:text-neutral-300">
       <div className="md:px-0 px-4">
         <a
           className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
@@ -30,6 +31,18 @@ export const SocialLinks = () => {
         >
           <XIcon size={14} />
           <p className="ml-2 h-7 hidden md:inline-block">yavorsky_</p>
+        </a>
+      </div>
+      <span className="mr-4 text-foreground/30">|</span>
+      <div className="md:px-0 px-4">
+        <a
+          className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://yavorsky.bsky.social/"
+        >
+          <BlueSkyIcon size={14} />
+          <p className="ml-2 h-7 hidden md:inline-block">yavorsky</p>
         </a>
       </div>
       <span className="mr-4 text-foreground/30">|</span>
