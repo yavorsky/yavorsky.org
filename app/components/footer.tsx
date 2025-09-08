f51c9533fc709b77b66848b4533ad3033697a2d8
Artem Yavorskyi <aqson@me.com>
2025-09-08 12:13:32 +0000

    fix: footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ import cx from 'classnames';
 
 export default function Footer({ className }: { className?: string }) {
   return (
-    <footer className={cx('pb-4', className)}>
+    <footer className={cx('pb-4 mt-4', className)}>
       <p className="text-neutral-600 dark:text-neutral-500">
         © {new Date().getFullYear()} MIT Licensed
       </p>
